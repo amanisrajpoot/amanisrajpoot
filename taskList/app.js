6c4f250a271c4e9f97da08545bde81fa6013cc8a
amanisrajpoot <amanrajpoot59@gmail.com>
2021-05-10 13:21:20 +0530

    UI Created and Main Variables Declared

---
+++ taskList/app.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-taks');
+const filter = document.querySelector('#filter');
+const takkInput = document.querySelector('#task');
+    
